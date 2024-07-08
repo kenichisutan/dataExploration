0895e852dcfade1371d4ac20156721a1ab8f41a7
kenichisutan <kenichi.jks@gmail.com>
2024-07-07 21:30:08 -0400

    Added better stop words to horizontal analysis.

--- horizontal/horizontal_analysis.py
+++ horizontal/horizontal_analysis.py
@@ -3,10 +3,16 @@ from collections import Counter
 import pandas as pd
 from matplotlib import pyplot as plt
 from nltk.corpus import stopwords
+from nltk import WordNetLemmatizer, word_tokenize
 import string
 import os
-
 from wordcloud import WordCloud
+import nltk
+
+# Download the necessary NLTK data files
+nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('punkt')
 
 
 def load_additional_stop_words(file_path):
@@ -20,6 +26,21 @@ additional_stop_words = load_additional_stop_words('../additional_stop_words.txt
 stop_words = set(stopwords.words('english'))
 stop_words.update(additional_stop_words)
 
+
+# Preprocessing functions
+def preprocess_text(text):
+    words = word_tokenize(text)
+    words = [word for word in words if word.lower() not in stop_words and word.isalpha()]
+    return ' '.join(words)
+
+
+def lemmatize_text(text):
+    words = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    words = [lemmatizer.lemmatize(word) for word in words]
+    return ' '.join(words)
+
+
 def load_and_process_data(paths):
     all_text_list = []
     for path in paths:
@@ -29,18 +50,22 @@ def load_and_process_data(paths):
             all_text_list.extend(df['content'].tolist())
         else:
             print(f"File not found: {path}")
-    return ' '.join(all_text_list)
+
+    processed_texts = []
+    for text in all_text_list:
+        if text.strip():  # Check if the text is not empty
+            preprocessed_text = preprocess_text(text)
+            lemmatized_text = lemmatize_text(preprocessed_text)
+            processed_texts.append(lemmatized_text)
+        else:
+            print(f"Skipping empty text in file")
+
+    return ' '.join(processed_texts)
+
 
 def most_common_words(all_text, name):
     print(f"Starting 20 most common words for {name}")
     all_text = all_text.translate(str.maketrans('', '', string.punctuation))
-
-    def preprocess_text(text):
-        words = text.split()
-        words = [word for word in words if word.lower() not in stop_words]
-        return ' '.join(words)
-
-    all_text = preprocess_text(all_text)
 
     words = all_text.split()
     word_freq = Counter(words)
@@ -56,24 +81,24 @@ def most_common_words(all_text, name):
     plt.close()
     print(f"Finished 20 most common words for {name}")
 
+
 def word_cloud(all_text, name):
     print(f"Starting word cloud for {name}")
     all_text = all_text.translate(str.maketrans('', '', string.punctuation))
 
-    def preprocess_text(text):
-        words = text.split()
-        words = [word for word in words if word.lower() not in stop_words]
-        return ' '.join(words)
+    wordcloud = WordCloud(width=800, height=800,
+                          background_color='white',
+                          stopwords=stop_words,
+                          min_font_size=10).generate(all_text)
 
-    all_text = preprocess_text(all_text)
-
-    wordcloud = WordCloud().generate(all_text)
-    plt.figure(figsize=(10, 6))
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    plt.savefig(f'./{name}_wordcloud.png')
+    plt.figure(figsize=(8, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout()
+    plt.savefig(f'./{name}_word_cloud.png')
     plt.close()
     print(f"Finished word cloud for {name}")
+
 
 # Paths to CSV files
 base_path = os.path.expanduser('~/MultiLayrtET2_Project/Data/2_proccessed_data_and_analysis/data/selected_data')
