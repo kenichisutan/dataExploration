dbd2b2f3dd31caf81e65267a96ccec2bcd0f2300
kenichisutan <kenichi.jks@gmail.com>
2024-07-27 14:47:01 -0400

    Removed redundant fields.

--- network/create_nodes.py
+++ network/create_nodes.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import os
-
 
 def load_data():
     # Load the CSV files
@@ -10,37 +8,25 @@ def load_data():
 
     return dominant_topics_df, article_date_df, entity_topic_sentiment_df
 
-
 def merge_data(dominant_topics_df, article_date_df, entity_topic_sentiment_df):
     # Merge the data on article_id
-    merged_df = entity_topic_sentiment_df.merge(dominant_topics_df, on='article_id').merge(article_date_df,
-                                                                                           on='article_id')
+    merged_df = entity_topic_sentiment_df.merge(dominant_topics_df, on='article_id').merge(article_date_df, on='article_id')
     return merged_df
 
-
-def create_nodes(merged_df):
-    # Create nodes based on entity, cluster, and topic
-    merged_df['node'] = merged_df.apply(lambda row: f"{row['entity']}_{row['cluster_id']}_{row['dominant_topic']}",
-                                        axis=1)
-    return merged_df
-
-
-def generate_node_ids(nodes_df):
+def generate_node_ids(merged_df):
     # Generate unique node IDs
-    nodes_df['node_id'] = nodes_df['node'].factorize()[0]
+    merged_df['node_id'] = merged_df['entity'].factorize()[0]
 
     # Reorder columns to have node_id as the leftmost column
-    cols = ['node_id'] + [col for col in nodes_df.columns if col != 'node_id']
-    nodes_df = nodes_df[cols]
+    cols = ['node_id'] + [col for col in merged_df.columns if col != 'node_id']
+    merged_df = merged_df[cols]
 
-    return nodes_df
-
+    return merged_df
 
 def save_network_to_csv(nodes_df, output_path):
     # Save the network to CSV
     nodes_df.to_csv(output_path, index=False)
     print(f"Network saved to {output_path}")
-
 
 def main():
     # Load the data
@@ -49,15 +35,14 @@ def main():
     # Merge the data
     merged_df = merge_data(dominant_topics_df, article_date_df, entity_topic_sentiment_df)
 
-    # Create nodes
-    nodes_df = create_nodes(merged_df)
+    # Remove unnecessary columns
+    merged_df = merged_df.drop(columns=['cluster_id', 'dominant_topic'])
 
     # Generate node IDs
-    nodes_df = generate_node_ids(nodes_df)
+    nodes_df = generate_node_ids(merged_df)
 
     # Save the network to CSV
     save_network_to_csv(nodes_df, 'entity_topic_network.csv')
-
 
 if __name__ == "__main__":
     main()
